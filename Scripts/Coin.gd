ee415672caa8a883757f4d6323acb499812e9fd3
ram <rramkumar1306@gmail.com>
2025-01-15 18:50:11 +0530

    Create Coin.gd

---
+++ Scripts/Coin.gd
@@ -0,0 +1,18 @@
+class_name Coin extends Area3D
+
+@onready var mesh = $Mesh
+
+var rotation_deg: float = 0.0
+
+func _ready():
+	rotation_deg = randf_range(45, 90)
+
+func _process(delta):
+	_rotate(delta)
+
+func _rotate(delta):
+	mesh.rotation_degrees.y -= (rotation_deg * delta) 
+
+
+func _on_body_entered(body):
+	print(body)
