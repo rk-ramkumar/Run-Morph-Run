d8a0da9b1c60c70906daa2d9f6d423d13b839288
ram <rramkumar1306@gmail.com>
2025-01-17 01:56:02 +0530

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,6 +8,11 @@ class_name Player extends CharacterBody3D
 const jumpVelocity = 20.0
 const lerpSpeed = 15.0
 
+enum STATE {
+	RUNNING,
+	SLIDING,
+	JUMPING
+}
 var animation_player: AnimationPlayer
 var speed = 15
 var direction = Vector3.ZERO
@@ -15,6 +20,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var swipe_start_position: Vector2 = Vector2.ZERO
 var swipe_end_position: Vector2 = Vector2.ZERO
 var min_swipe_distance: float = 50.0
+var current_state: STATE = STATE.RUNNING
 
 func _ready():
 	if !armature_scene:
@@ -31,8 +37,14 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
+	# Enable collision when player land
 	if is_on_floor() and leg_hitbox.disabled:
 		leg_hitbox.disabled = false
+		current_state = STATE.RUNNING
+	
+	if animation_player.current_animation == "Running" and head_hitbox.disabled:
+		head_hitbox.disabled = false
+		current_state = STATE.RUNNING
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -69,28 +81,34 @@ func _handle_movement():
 			_move_up()
 
 func _move_right():
+	if current_state == STATE.SLIDING:
+		return
 	var new_pos = clamp(position.x - 2.5, -2.5, 0)
 	position.x = new_pos
 
 func _move_left():
+	if current_state == STATE.SLIDING:
+		return
 	var new_pos = position.x + 2.5
 	position.x = clamp(new_pos, 0, 2.5)
 
 func _move_down():
 	if not is_on_floor():
 		return
-
+	current_state = STATE.SLIDING
+	head_hitbox.disabled = true
 	play_animation("Slide")
 
 func _move_up():
 	if not is_on_floor():
 		return
-
+	current_state = STATE.JUMPING
 	velocity.y = lerp(velocity.y, jumpVelocity, get_physics_process_delta_time() * lerpSpeed)
 	leg_hitbox.disabled = true
 	play_animation("Jump")
 
 func play_animation(anim_name):
 	animation_player.play(anim_name)
-	animation_player.animation_set_next(anim_name, "Running")
+	animation_player.clear_queue()
+	animation_player.queue("Running")
 	
