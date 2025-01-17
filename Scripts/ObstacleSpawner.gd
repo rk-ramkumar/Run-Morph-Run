eefe9886b21f6e1ea62c8637e914030c5a9b67bf
ram <rramkumar1306@gmail.com>
2025-01-18 00:14:13 +0530

    Create ObstacleSpawner.gd

---
+++ Scripts/ObstacleSpawner.gd
@@ -0,0 +1,4 @@
+class_name ObstacleSpawner extends Node
+
+var obstacle_down: PackedScene = preload("res://Scenes/Obstacles/obstacles_down.tscn")
+var obstacle_up: PackedScene = preload("res://Scenes/Obstacles/obstacles_up.tscn")
