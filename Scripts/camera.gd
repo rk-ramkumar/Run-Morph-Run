b8d471fc3d35b936009d529a92b16a1c4b143485
ram <rramkumar1306@gmail.com>
2025-01-25 15:13:46 +0530

    Create camera.gd

---
+++ Scripts/camera.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+@export var player: Player
+
+func _ready():
+	GameManager.game_start.connect(_on_game_start)
+	if player:
+		player.lane_changed.connect(_on_lane_changed)
+
+func _on_game_start():
+	position.x = 0
+
+func _on_lane_changed(x_pos):
+	smooth_move_camera(x_pos)
+
+func smooth_move_camera(target_x_pos: float):
+	var tween = create_tween()
+	tween.tween_property(self, "position:x", target_x_pos, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
