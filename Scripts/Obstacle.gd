36f94416e214d0946a2b81dbbddbb0857e833b45
ram <rramkumar1306@gmail.com>
2025-01-26 05:35:03 +0530

    Update Obstacle.gd

--- Scripts/Obstacle.gd
+++ Scripts/Obstacle.gd
@@ -2,5 +2,7 @@ extends Area3D
 
 
 func _on_body_entered(body):
-	if body is Player and body.position.x == position.x:
+	if body is Player:
+		if position.z < -0.5 or body.position.x != position.x:
+			return
 		GameManager.register_collision()
